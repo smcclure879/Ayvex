f5468906f605eefd463c3e4deb12e0ab80231e7e
smcclure879 <smcclure879@yahoo.com>
2013-11-24 17:10:57 -0800

    modified:   onStartup.pl

--- projects/tdns/onStartup.pl
+++ projects/tdns/onStartup.pl
@@ -1,25 +1,30 @@
 #!/usr/bin/perl
 use strict;
 use LWP::Simple;
-my $content = get "http://checkip.dyndns.org/";
-#print $content;
-$content =~ /(\d{1,3}\.){3}\d{1,3}/gio;
-my $ipAddr = $&;
-
-print "found ip $ipAddr\n";
-
 my $mcLocalFolder = "C:\\Users\\steve\\Documents\\GitHub\\Ayvex\\web\\mc\\";
 my $mcGuideFile = 'guide.htm';
-##my $r = Git::Repository->new( work_tree => $mcLocalFolder );
 
+
+# get IP addr
+my $content = get "http://checkip.dyndns.org/";
+$content =~ /(\d{1,3}\.){3}\d{1,3}/gio;
+my $ipAddr = $&;
+print "found ip $ipAddr\n";
+
+##my $r = Git::Repository->new( work_tree => $mcLocalFolder );
 print system(qq(git checkout $mcLocalFolder$mcGuideFile));
 
 open my $fhw, ">", "$mcLocalFolder$mcGuideFile";
 
-print $fhw "my ip is 658bugbug:   $ipAddr\n\n";
+print $fhw "my ip is bugbug:   $ipAddr\n\n";
+print $fhw "got that at "+time;
+print "got that at "+time;
 
 close $fhw;
 
 
-print system(qq(git commit -C $mcLocalFolder$mcGuideFile ));
+print system(qq(git commit -m "foobar" $mcLocalFolder$mcGuideFile ));
+print system(qq(git push ));
+print "file will appear at https://raw.github.com/smcclure879/Ayvex/master/web/mc/guide.htm"
+
 
