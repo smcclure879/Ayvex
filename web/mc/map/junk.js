8ba6457563787bef6d46bf4cf7af4e449e7a3fdd
steve mcclure <smcclure879@yahoo.com>
2013-12-30 03:48:03 -0600

    new file:   cameraAndStuff.js 	new file:   csvNotUsed.js 	new file:   demoUtils.js 	new file:   junk.js 	new file:   mc2dad1.js 	new file:   pre3d.js 	new file:   theMap.html

---
+++ web/mc/map/junk.js
@@ -0,0 +1,31 @@
+<script>
+<!-- 	var canvas = document.getElementById("c");
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle="beige";
+	ctx.lineStyle="black";
+	//ctx.fillRect(0,0,799,599);
+	ctx.strokeRect(128, 96, 64, 48);
+
+		  
+	ctx.font='30px Arial';
+	ctx.fillStyle="black";
+	ctx.fillText('How are you today?',20,50);
+		
+	ctx.beginPath(); 
+	ctx.lineWidth="5";
+	ctx.strokeStyle="green"; // Green path
+	ctx.moveTo(0,75);
+	ctx.lineTo(250,75);
+	ctx.stroke(); // Draw it
+	
+	
+	
+	//ctx.fillText("Hello World!",10,50);
+
+	//ctx.font = "bold 12px sans-serif";
+	ctx.fillText("x", 248, 43);
+	ctx.fillText("y", 58, 165);
+	
+  document.write("end")
+  -->
+ </script>
