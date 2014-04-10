303cd2e87b05168b990672a732610dcf26a9894c
steve mcclure <smcclure879@yahoo.com>
2014-04-10 02:35:49 -0700

    this version runs remote against server.  can it run local on the server? 	modified:   go.py

--- webStarter/go.py
+++ webStarter/go.py
@@ -1,32 +1,29 @@
-import os
+import os,sys
 import json
 import httplib
 import urllib
+import pprint
+import time
+from base64 import b64encode
 
-"""{
-  "_id":"attachment_doc",
-  "_attachments":
-  {
-    "foo.txt":
-    {
-      "content_type":"text\/plain",
-      "data": "VGhpcyBpcyBhIGJhc2U2NCBlbmNvZGVkIHRleHQ="
-    }
-  }
-}"""
 
-#print json.dumps({'foo':'bar'})
+if len(sys.argv)<1:
+	raise "must provide name:pwd"
+userPass=b""+sys.argv[1]
+serverPort="192.168.1.110:5984"
+designDoc="/cosmos/_design/passthru"
+
+
 def firstFoundVal(obj,listOfKeys):
 	for k in listOfKeys:
 		if (k in obj):
 			return obj[k]
 	print "no key found"
+	pprint.pprint(obj)
 	raise "no key found"
 
 def extractRev(obj):
 	return firstFoundVal(obj,['rev','_rev','etag']);			
-
-
 	
 def getFile(file):
 	content='bad'
@@ -38,17 +35,17 @@ def getMimeType(file):
 		return "text/javascript"
 	if (file.endswith(".html")):
 		return "text/html"
+	if (file.endswith(".htm")):
+		return "text/html"
 	if (file.endswith(".gif")):
 		return "image/gif"
 	if (file.endswith(".jpg")):
 		return "image/jpeg"
 	return "application/octet-stream";
 
-
-		
 def getRev():
-	conn = httplib.HTTPConnection("127.0.0.1:5984")
-	conn.request("GET","/cosmos/_design/passthru")
+	conn = httplib.HTTPConnection(serverPort)
+	conn.request("GET",designDoc)
 	res = conn.getresponse()
 	print res.status, res.reason
 	data = res.read()
@@ -56,8 +53,6 @@ def getRev():
 	print data
 	obj=json.loads(data)
 	return extractRev(obj)
-
-	
 	
 def allRelevantFilesUnder(startDir):
 	howMuchToRemoveFromStartOfPaths=len(startDir)
@@ -72,34 +67,37 @@ def allRelevantFilesUnder(startDir):
 		# for name in dirs:
 			# pass
 			#print(os.path.join(root, name))
-
-			
-	
 			
 def putAttachment(filePath,relativeUrl):
 	rev=getRev()  #optimize this later
 	print "prev rev="+rev
-	editUrl="/cosmos/_design/passthru/"+url+"?rev="+urllib.quote(rev)
+	editUrl=designDoc+'/'+relativeUrl+"?rev="+urllib.quote(rev)
 	print editUrl
 	body=getFile(filePath)
 	mimeType=getMimeType(filePath)
-	conn = httplib.HTTPConnection("127.0.0.1:5984")
-	conn.request("PUT",editUrl,body,{'Content-type':mimeType})
+	userAndPass = b64encode(userPass).decode("ascii")
+	conn = httplib.HTTPConnection(serverPort)
+	conn.request("PUT",editUrl,body,
+			{
+				'Content-type':mimeType
+				,				'Authorization' : 'Basic %s' %  userAndPass 
+				
+			}
+		)
 	res = conn.getresponse()
-	print res.status, res.reason
+	#print res.status, res.reason
 	data = res.read()
 	print len(data)
-	print data
+	#print data
 	obj=json.loads(data)
 	return extractRev(obj)
 
-#putAttachment("testFile.txt")
-
-# for f in allRelevantFilesUnder("..\\web"):
-	# print f
 
 	
 items=allRelevantFilesUnder("..\\web")
 for file,url in items:
+	while url.startswith("/"):
+		url=url[1:]
 	print "finished"+putAttachment(file,url)
+	time.sleep(1)
 
