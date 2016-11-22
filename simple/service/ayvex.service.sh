0e50ae58c6c5d2000ab125fc2bf8ec258eaf05c4
steve mcclure <smcclure879@yahoo.com>
2016-11-21 19:10:51 -0800

    serivce works, i think, just not autostart maybe

---
+++ simple/service/ayvex.service.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# note should be copied into  /etc/init.d/ayvex
+
+### BEGIN INIT INFO
+# Provides: ayvex
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Simple script to start a program at boot bugbug
+# Description: A simple script from www.stuffaboutcode.com which will start / stop a program a boot / shutdown.
+### END INIT INFO
+
+
+#if you want a command to always run, put it here
+
+
+
+case "$1" in
+start)
+echo "starting ayvex"
+nodejs  /home/pi/gitstuff/ayvex/simple/simple.server.node.js
+;;
+stop)
+echo "Stopping ayvex"
+killall nodejs
+#bugbug wrap up nodejs in an ayvex process
+;;
+*)
+echo "Usage: /etc/init.d/ayvex {start|stop}"
+exit 1
+;;
+esac
+echo "ayvex out"
+exit 0
+
--- simple/service/setupService.sh
+++ simple/service/setupService.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-
-cp ayvex.service /lib/systemd/system
-chmod 644 /lib/systemd/system/ayvex.service
-chmod +x /home/pi/gitstuff/ayvex/simple/simple.server.node.js
-
-
-
-
+#!/bin/sh
+cp ayvex.service.sh /etc/init.d/ayvex
+chmod a+x /etc/init.d/ayvex
+chmod a+x /home/pi/gitstuff/ayvex/simple/simple.server.node.js
