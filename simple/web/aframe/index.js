8a4e52a47effc8db3ef880e2cae41b6c8e4d5717
ayvex llc <smcclure897@yahoo.com>
2017-01-21 01:59:19 -0800

    gaze selection sorta works, first try!
    	modified:   index.html
    	modified:   index.js

--- simple/web/aframe/index.js
+++ simple/web/aframe/index.js
@@ -29,6 +29,19 @@ $("document").ready( function(event) {
     //    alert("447bugbug");
     //},2000);
     
+    
+    // Component to change to random color on click.
+    AFRAME.registerComponent('cursor-listener', {
+      init: function () {
+        var COLORS = ['red', 'green', 'blue'];
+        this.el.addEventListener('click', function (evt) {
+          var randomIndex = Math.floor(Math.random() * COLORS.length);
+          this.setAttribute('material', 'color', COLORS[randomIndex]);
+          console.log('I was clicked at: ', evt.detail.intersection.point);
+        });
+      }
+    });
+    
     //all the conference stuff in one place for now
     $remoteVideo=document.querySelector("#remoteVideo");
     $localVideo=document.querySelector("#localVideo");
