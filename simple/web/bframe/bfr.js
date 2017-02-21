aaaf903b79e01789c93d9fc291f624e150456324
ayvex llc <smcclure897@yahoo.com>
2017-02-21 01:32:14 -0800

    wip

--- simple/web/bframe/bfr.js
+++ simple/web/bframe/bfr.js
@@ -9,29 +9,44 @@
 // }
 
 
-
+// typical usage.....
+//     ja.blank.green().platform().mixin("something").color('blue')
 
 class ja {
-
-    constructor() {
-	this.und = document.createElement('a-entity');
-    }  //understood "return this"
-
-    // typical usage.....
-    //     ja.blank.green().platform().mixin("something").color('blue')
-
 
     static get blank() {
 	return new ja();
     }
+    
+    constructor() {
+	this.und = document.createElement('a-entity');
+    }  //understood "return this"
+
+
+
+    static get testblank() {
+	//var retval=document.createElement('a-entity');
+	var retval = new ja();
+	retval.und.setAttribute('geometry','primitive: cone; height:7; radiusTop:0, radiusBottom:0.25');
+	retval.und.setAttribute('material','color','orange');
+	retval.und.setAttribute('cursor-listener',{});
+	return retval;
+    }
+
+    
+
 
     get box() { 
 	this.und.setAttribute("geometry","primitive","box");
-	this.und.setAttribute("depth","2");
-	this.und.setAttribute("width","2");
-	this.und.setAttribute("height",2);
-	this.und.setAttribute("scale","1 1 1");
-	return this.wire;
+	this.und.setAttribute("id","barney");
+	this.und.setAttribute("wireframe",true);
+	this.und.setAttribute("material","color","red");
+	this.und.setAttribute("position",{x:0, y:0, z:0});
+	//this.und.setAttribute("geometry","depth",2);
+	//this.und.setAttribute("geometry","width",2);
+	//this.und.setAttribute("geometry","height",2);
+	//this.und.setAttribute("scale","1 1 1");
+	return this;
     }
 
 
@@ -64,20 +79,27 @@ class ja {
 	return this;
     }
     
-    
-
     into(dest) {
-	if (typeof dest === 'function')
-	    return dest(this.und); 
-	//else if (typeof x === 'string') {  //then make it a parent entity!
-	//    alert("trying the thing bugbug1259i");
-	//    return this.into(document.querySelector("#"+x).appendChild);
-	//}
-	else 
-	    alert("bugbug1217c");
-	
-
+	var result = this._into(dest);
+	if (result!=0)
+	    alert("bugbug708:" + result);
 	return this;
+    }
+
+    _into(dest) {
+	if (typeof dest != 'function') {
+	    return "bugbug1217c";
+	}
+	// if (typeof this != 'ja') {
+	//     return "bugbug1138u"+JSON.stringify(this);
+	// }
+	//if (typeof this.und != "a-entity") {
+	//    return "bugbug1139i"+typeof this.und;
+	//}
+	
+	//debugger; //bugbug you are here
+	dest.call(null, this.und);
+	return 0;
     }
 
 
@@ -99,22 +121,32 @@ $("document").ready( function(event) {
 
     var foo = document.querySelector("#foo");
     //var collector =  (x)=>{foo.appendChild(x);};  //fnPointer  bugbug simplify
-    var collector = (x)=>{
-	x.flushToDOM();
+    var collector = function(x){
+	foo.appendChild(x);
+	x.flushToDOM(false);
 	console.log(x);
     };
-    //for this experiment all the following should work...
-    ja.blank.green.into(collector);  //green.box.pos("-11 3 -3").into(collector);
-    //scale("1 1 1").id("foo2").into(collector);
 
-    //bugbug
-    //afh.blank().box().position("1 1 1").scale("44 44 44").green().useWith(collect);
-    //afh.blank().green().platform().mixin("something").color('blue').useWith(collect);
+
+
+    setTimeout(function(){
+	//for this experiment all the following should work...
+	//ja.blank.box.into(collector);  //green.box.pos("-11 3 -3").into(collector);
+	//scale("1 1 1").id("foo2").into(collector);
+	
+	//bugbug
+	//afh.blank().box().position("1 1 1").scale("44 44 44").green().useWith(collect);
+	//afh.blank().green().platform().mixin("something").color('blue').useWith(collect);
+
+	//bugbug not even this????
+	ja.testblank.into(collector);
+
+    },2000);
 });
 		     
 
 
-
+//keep as reference....worked in other code!
 // function createBlankUser() {
 //     var retval=document.createElement('a-entity');
 //     retval.setAttribute('geometry','primitive: cone; height:7; radiusTop:0, radiusBottom:0.25');
@@ -150,50 +182,3 @@ $("document").ready( function(event) {
     // });
     // user.appendChild(label);   
 
-
-
-function copyAttribute(trg,src,name) {
-    var val=src.getAttribute(name);
-    trg.setAttribute(name,val);
-}
-
-
-
-
-function runIfFunction(f,arg) {
-    if (typeof f != 'function') 
-	return null;
-    
-    return f(arg);
-}
-
-
-
-var allowedAttribs=['position','rotation','scale','id','visible'];
-function purify(obj) {
-    
-    //obj.removeAllChildren();
-    while (obj.hasChildNodes()) {
-	obj.removeChild(obj.lastChild);
-    }
-    
-    //obj.removeAllNonAllowedAttributes()...
-    for (var ii = 0; ii < obj.attributes.length; ii++) {
-	var attrib = obj.attributes[ii];
-	if (!attrib.specified) continue;
-	if (allowedAttribs.indexOf(attrib.name)<0) continue;
-	if (attrib.name.startsWith('_')) continue;
-
-	obj.attributes.removeNamedItem(attrib.name);
-        //console.log(attrib.name + " = " + attrib.value);
-    }
-
-}
-
-
-
-function hydrate(){
-
-
-
-}
