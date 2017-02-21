d760e373167750ac3f7ed21a51d796e25966e8f3
ayvex llc <smcclure897@yahoo.com>
2017-02-21 02:11:48 -0800

    working example of domain specific language for forming up complex entities in a-frame.

--- simple/web/bframe/bfr.js
+++ simple/web/bframe/bfr.js
@@ -1,22 +1,8 @@
-// class Model {
-//   constructor(properties) {
-//     this.properties = properties;
-//   }
-
-//   toObject() {
-//     return this.properties;
-//   }
-// }
-
-
 // typical usage.....
 //     ja.blank.green().platform().mixin("something").color('blue')
 
 class ja {
 
-    static get blank() {
-	return new ja();
-    }
     
     constructor() {
 	this.und = document.createElement('a-entity');
@@ -25,105 +11,28 @@ class ja {
 
 
     static get testblank() {
-	//var retval=document.createElement('a-entity');
 	var retval = new ja();
 	retval.und.setAttribute('geometry','primitive: cone; height:7; radiusTop:0, radiusBottom:0.25');
 	retval.und.setAttribute('material','color','orange');
-	retval.und.setAttribute('cursor-listener',{});
+	retval.und.setAttribute("position",{x:0, y:0, z:0});
 	return retval;
     }
-
     
+    into(destFn) {
+	if (typeof destFn != 'function') 
+	    alert( "bugbug1217c" );
 
-
-    get box() { 
-	this.und.setAttribute("geometry","primitive","box");
-	this.und.setAttribute("id","barney");
-	this.und.setAttribute("wireframe",true);
-	this.und.setAttribute("material","color","red");
-	this.und.setAttribute("position",{x:0, y:0, z:0});
-	//this.und.setAttribute("geometry","depth",2);
-	//this.und.setAttribute("geometry","width",2);
-	//this.und.setAttribute("geometry","height",2);
-	//this.und.setAttribute("scale","1 1 1");
-	return this;
+	destFn(this.und);
     }
-
-
-    pos(po) {
-	this.und.setAttribute("position","position",po);
-	return this;
-    }
-    
-    scale(s) {
-	this.und.setAttribute("scale","scale",s);
-	return this;
-    }
-
-    id(x) {
-	this.und.setAttribute("id",x);
-	return this;
-    }
-
-    color(co) {
-	this.und.setAttribute("material","color",co);
-	return this;
-    }
-
-    get green() {
-	return this.color("green");
-    }
-
-    get wire() {
-	this.und.setAttribute("wireframe","true"); 
-	return this;
-    }
-    
-    into(dest) {
-	var result = this._into(dest);
-	if (result!=0)
-	    alert("bugbug708:" + result);
-	return this;
-    }
-
-    _into(dest) {
-	if (typeof dest != 'function') {
-	    return "bugbug1217c";
-	}
-	// if (typeof this != 'ja') {
-	//     return "bugbug1138u"+JSON.stringify(this);
-	// }
-	//if (typeof this.und != "a-entity") {
-	//    return "bugbug1139i"+typeof this.und;
-	//}
-	
-	//debugger; //bugbug you are here
-	dest.call(null, this.und);
-	return 0;
-    }
-
-
 }
 
 
 
-
-////////////////////////
-
-
-
-
-
-
 $("document").ready( function(event) {
-
-    //var urlParams = new URLSearchParams(window.location.search);
-
-    var foo = document.querySelector("#foo");
-    //var collector =  (x)=>{foo.appendChild(x);};  //fnPointer  bugbug simplify
+    var foo = document.querySelector("#scene");  //bugbug or #foo??
     var collector = function(x){
 	foo.appendChild(x);
-	x.flushToDOM(false);
+	x.flushToDOM(true);
 	console.log(x);
     };
 
@@ -139,7 +48,7 @@ $("document").ready( function(event) {
 	//afh.blank().green().platform().mixin("something").color('blue').useWith(collect);
 
 	//bugbug not even this????
-	ja.testblank.into(collector);
+    ja.testblank.into(collector);
 
     },2000);
 });
