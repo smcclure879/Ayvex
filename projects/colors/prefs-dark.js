05b41dc3c0e23d7af952b1394e449da257876bc6
Ayvex Light Industries <smcclure879@yahoo.com>
2017-11-12 00:51:25 -0800

    wip to fix colors for my eyes and day and nite use on the chromebook
    	new file:   prefs-dark.js
    	new file:   prefs-lite.js
    	new file:   seeColors.sh

---
+++ projects/colors/prefs-dark.js
@@ -0,0 +1,28 @@
+// Disable bold 
+term_.prefs_.set('enable-bold', false) 
+
+// Use this for Solarized Dark 
+term_.prefs_.set('background-color', "#002b36");
+term_.prefs_.set('foreground-color', "#839496");
+
+// Solarized Colors
+term_.prefs_.set('color-palette-overrides', [
+
+    '#fdf6e3',
+    '#93a1a1', 
+    '#6c71c4', 
+    '#839496', 
+    '#657b83', 
+    '#586e75', 
+    '#cb4b16', 
+    '#002b36', 
+    '#eee8d5', 
+    '#2aa198', 
+    '#d33682', 
+    '#268bd2', 
+    '#b58900',
+    '#859900', 
+    '#dc322f', 
+    '#073642', 
+]);
+
