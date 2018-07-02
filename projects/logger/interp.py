e16cb76818458115cb74293f57885fa1a902fc82
steve mcclure <smcclure879@yahoo.com>
2018-07-02 06:25:06 +0000

    results on altitudes and pressures
    	modified:   finalRiseData.tsv
    	modified:   finalRiseFromLodge.nb
    	modified:   interp.py
    	new file:   monday.nb
    	new file:   mondayI.tsv
    	new file:   overfin.tsv

--- projects/logger/interp.py
+++ projects/logger/interp.py
@@ -1,5 +1,5 @@
 import math,os,sys,io
-
+import fileinput
 
 def row(*args):
     sys.stdout.write("\t".join(map(str,args)))
@@ -7,7 +7,7 @@ def row(*args):
 
 c=0
 prev=0
-with open("riseFromLodge.txt") as f:
+with fileinput.input() as f:
     for x in f:
         #x=f.readline()
         if len(x)<8 or x.startswith('new'):
@@ -15,6 +15,19 @@ with open("riseFromLodge.txt") as f:
         x=x.split(",")
         #print(x[4:9])
         try:
+            time1=x[0]
+            if time1[:4] != "2018":
+                continue
+            year=int(time1[:4])
+            mo=int(time1[5:7])
+            day=int(time1[8:10])
+            hr=int(time1[11:13])
+            minx=int(time1[14:16])
+            sec=int(time1[17:19])
+            
+            timecode= hr*3600 + minx*60 + sec
+
+
             alt1=float(x[4][:-1])
             alt2=float(x[5][:-1])+float(x[6])
             lat=x[7]
@@ -25,14 +38,14 @@ with open("riseFromLodge.txt") as f:
 
             impact=abs((alt2-prev)/alt2)
             prev=alt2
-            thresh=0.01
+            thresh=0.03
             #print(abs(impact))
             
-            if  c % 200 == 0  :
-                row("1",c,lat,lon,alt2,alt1)
+            if  c % 120 == 0  :
+                row("byCount",timecode,lon,lat,alt2,alt1)
                 pass
             elif  impact > thresh :
-                row("2",c,lat,lon,alt2,alt1)
+                row("byImpact",timecode,lon,lat,alt2,alt1)
                 
         except Exception as ex:
             #print(ex)
