1ea94c571192f9230c783a67f18db61151b2d3f1
smcclure879 <smcclure879@yahoo.com>
2019-03-19 15:53:15 -0700

    wip: works from command line to set DNS not automatic yet.
    	new file:   updateDnsEntries.node.js

---
+++ server/dns/updateDnsEntries.node.js
@@ -0,0 +1,108 @@
+const https = require('https');
+const fs = require('fs');
+const dns = require('dns');
+const pathlib = require('path');
+
+//it is mar2019 and still having issues with dns
+
+//but we have nodejs running automatically and real nice.  so rewrite in js.
+
+//1. do google domains only at first.  dyndns later
+//2. try to find free dns.  and/or reverse proxy
+
+
+async function grabx(url,label) {
+    if (!label) label = '';
+    return new Promise( (resolve) => {
+	https.get(url, (resp) => {
+	    let data = '';
+	    resp.on('data', (chunk) => {
+		data += chunk;
+		console.log("chunk:"+chunk+"     "+label);
+	    });
+
+	    resp.on('end', () => {
+		console.log("resolving:"+label);
+		resolve(data);
+	    });
+
+	}).on("error", (err) => {
+	    console.log("Error: " + err.message);
+	    throw err;
+	});
+    });
+}
+
+
+async function setIpForDomain(dom,ip){
+    var url = "https://"+dom.user+":"+dom.pazz+"@domains.google.com/nic/update?hostname="+dom.name;
+    // optional........+"&myip="+ip
+
+    var result = await grabx( url ,"setIp for "+dom.name );
+    console.log("result="+result);
+}
+    
+
+
+
+async function getActualIp() {
+    return grabx("https://domains.google.com/checkip","checkip");
+}
+
+
+async function getDnsIp(dnsName) {
+    return new Promise( (resolve) => {
+	dns.lookup(dnsName, (err, address, family) => {
+	    resolve(address);
+	});
+    });
+}
+
+
+const pazzFile = "realPazzFile.noCheckin.pazz";
+
+
+async function readJsonFile(path) {
+    return new Promise( (resolve) => {
+	fs.readFile(path, 'utf8', function (err, data) {
+	    if (err) throw err;
+	    var obj = JSON.parse(data);
+	    resolve(obj);
+	});
+    });
+}
+
+function canonicalize(path) {
+    return pathlib.normalize(path);
+}
+
+
+async function main() {
+    var actualIp = await getActualIp();
+    var domainConfig = await readJsonFile(canonicalize(pazzFile));
+
+    for (var domain of domainConfig) {
+	console.log("----------"+domain.name+"---------");
+	var dnsIp = await getDnsIp(domain.name);
+	if (dnsIp != actualIp ) {
+	    console.log("dns="+dnsIp);
+	    console.log(actualIp);
+	    setIpForDomain( domain, actualIp );
+	} else {
+	    console.log("***OK***");
+	}
+    }
+}
+
+
+
+
+/*
+async function executeAsyncTask () {
+    const valueA = await functionA()
+    const valueB = await functionB(valueA)
+    return function3(valueA, valueB)
+}
+*/
+
+main();
